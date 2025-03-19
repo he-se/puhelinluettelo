bf873f2baf4ba3c0b7a292e7c34461fdffc8d9ed
heimat <heikki.mattila@rpkk.fi>
2025-03-19 10:50:38 +0200

    teht 3.22
    Part 3 valmis.

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-// const morgan = require("morgan");
-const Person = require("./models/person"); // Person-konstruktorifunktio
+const Person = require("./models/person"); // Person -konstruktorifunktio
 
 const app = express();
-
-// const url = `mongodb+srv://heikki:${password}@cluster0.hskrx.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`;
-
-// let persons = [
-//   {
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//     id: "1",
-//   },
-//   {
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//     id: "2",
-//   },
-//   {
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//     id: "3",
-//   },
-//   {
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//     id: "4",
-//   },
-// ];
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -55,15 +29,6 @@ app.use(express.static("dist")); // Show static pages
 app.use(express.json());
 app.use(requestLogger);
 
-// morgan.token("data", (req) => {
-//   //creating data token
-//   return JSON.stringify(req.body);
-// });
-
-// app.use(
-//   morgan(":method :url :status :res[content-length] - :response-time ms :data")
-// );
-
 // Routes
 app.get("/info", (request, response) => {
   Person.find({}).then((persons) => {
@@ -78,20 +43,7 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-// const generateId = () => {
-//   const id = Math.floor(Math.random() * 1000000);
-//   return id.toString();
-// };
-
 app.get("/api/persons/:id", (request, response, next) => {
-  // const id = request.params.id;
-  // const person = persons.find((person) => person.id === id);
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   console.log("x");
-  //   response.status(404).end();
-  // }
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -110,7 +62,6 @@ app.post("/api/persons", (request, response, next) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    // id: generateId(),
   });
 
   // Save to db -> http response (toJSON formatted) if succeeded
@@ -142,10 +93,6 @@ app.put("/api/persons/:id", (request, response, next) => {
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
-  // const id = request.params.id;
-  // persons = persons.filter((person) => person.id !== id);
-
-  // response.status(204).end();
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
       response.status(204).end();
